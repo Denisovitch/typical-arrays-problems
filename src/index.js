e33db01b6902f38b4c024b9675472c26e957c1db
Denisovitch <denisglazin@gmail.com>
2021-08-23 18:09:46 +0300

    task completed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
+exports.min = function min(array) {
+    let min = 0;
+    if (array && array.length > 0) {
+        min = Math.min.apply(Math, array);
+        return min;
+    }
 
-exports.min = function min (array) {
-  return 0;
-}
+    return min;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    let max = 0;
+    if (array && array.length > 0) {
+        max = Math.max.apply(Math, array);
+        return max;
+    }
 
-exports.avg = function avg (array) {
-  return 0;
-}
+    return max;
+};
+
+exports.avg = function avg(array) {
+    let avg = 0;
+    let total = 0;
+    if (array && array.length > 0) {
+        for (let i = 0; i < array.length; i++) {
+            total += array[i];
+        }
+        avg = total / array.length;
+    }
+
+    return avg;
+};
